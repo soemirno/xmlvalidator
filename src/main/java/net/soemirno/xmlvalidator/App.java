18654d070322ceba5082e095346b33563e37f37e
soemirno <soemirno@gmail.com>
2009-02-13 16:15:48 +0100

    added build reports

--- src/main/java/net/soemirno/xmlvalidator/App.java
+++ src/main/java/net/soemirno/xmlvalidator/App.java
@@ -2,11 +2,9 @@ package net.soemirno.xmlvalidator;
 
 /**
  * Hello world!
- *
  */
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
