c39db1c371d2087c9a9a9d4177c0d791c7d53612
soemirno <soemirno@gmail.com>
2009-02-13 16:14:26 +0100

    added build reports

---
+++ src/main/java/net/soemirno/xmlvalidator/App.java
@@ -0,0 +1,12 @@
+package net.soemirno.xmlvalidator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/net/soemirno/xmlvalidator/AppTest.java
@@ -0,0 +1,14 @@
+package net.soemirno.xmlvalidator;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @org.junit.Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
