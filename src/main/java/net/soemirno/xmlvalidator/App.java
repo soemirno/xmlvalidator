6c03c8ec1c31774cf2bd83d1adcdfab7b711daa3
soemirno <soemirno@gmail.com>
2009-02-13 16:47:29 +0100

    call validator using files created from commandline arguments

--- src/main/java/net/soemirno/xmlvalidator/App.java
+++ src/main/java/net/soemirno/xmlvalidator/App.java
@@ -1,10 +1,19 @@
 package net.soemirno.xmlvalidator;
 
+import java.io.File;
+
 /**
- * Hello world!
+ * Responsible for starting application.
  */
 public class App {
+
+    private static XmlValidator validator;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        validator.validate(new File(args[0]), new File(args[1]));
+    }
+
+    public static void setValidator(XmlValidator aValidator) {
+        validator = aValidator;
     }
 }
--- src/test/java/net/soemirno/xmlvalidator/AppTest.java
+++ src/test/java/net/soemirno/xmlvalidator/AppTest.java
@@ -1,14 +1,37 @@
 package net.soemirno.xmlvalidator;
 
-import static org.junit.Assert.assertTrue;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(JMock.class)
 public class AppTest {
 
-    @org.junit.Test
-    public void testApp() {
-        assertTrue(true);
+    Mockery context = new JUnit4Mockery();
+    private XmlValidator validator;
+
+    @Before
+    public void setUp() {
+        validator = context.mock(XmlValidator.class);
+        App.setValidator(validator);
+    }
+
+    @Test
+    public void shouldCallValidatorWithArgValues() {
+
+        context.checking(new Expectations() {{
+            oneOf(validator).validate(with(equal(new File("schemaname"))), with(equal(new File("xmlfile"))));
+        }});
+
+        App.main(new String[]{"schemaname", "xmlfile"});
     }
 }
